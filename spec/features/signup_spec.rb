4c2eaaf74220cfce65bf69ce6830ae6df3dadc1b
manisaguana <armanisaldana@gmail.com>
2016-09-12 13:18:41 -0700

    Wrote invalid sign up feature test

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -16,4 +16,18 @@ describe "The sign up procces", type: :feature do
     expect(user).to exist
   end
 
+  it "fails and raises an error if invalid information is entered", js: true, type: :feature do
+    def invalid_signup
+      visit "/signup" 
+
+      fill_in "Username",         with: "username"
+      fill_in "Email"   ,         with: "usergmailcom"
+      fill_in "Password",         with: "pass"
+      fill_in "Confirm Password", with: "pass1234"
+      click_button "Create User"
+    end
+
+    expect(invalid_signup).to raise_error
+  end
+
 end
