28428a73c8716007fa7e10ce2f6a7e6ff87c031e
manisaguana <armanisaldana@gmail.com>
2016-10-11 13:42:50 -0700

    Cleaned up message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008045541) do
+ActiveRecord::Schema.define(version: 20161011203219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 20161008045541) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
-    t.integer  "author_id"
-    t.integer  "chat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
