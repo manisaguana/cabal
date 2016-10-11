0c3a6e29aca6a391ee4eb3deadd38bc9afe0b716
manisaguana <armanisaldana@gmail.com>
2016-10-11 14:53:25 -0700

    Implemented basic functionality (needs styling)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      redirect_to root_url
+      redirect_to rooms_show_path
     else
       render "new"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
+
   get 'sessions/new'
 
   root 'home#index'
