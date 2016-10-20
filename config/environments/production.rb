616db9df1a85d856380e0c4f97471543af1f9d4e
manisaguana <armanisaldana@gmail.com>
2016-10-19 19:47:47 -0700

    mount path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
+  config.action_cable.mount_path = '/cable' 
   config.action_cable.url = 'wss://cabal.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://cabal.herokuapp.com', 'https://cabal.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://cabal.herokuapp.com', 'http://cabal.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
